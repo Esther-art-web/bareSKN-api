2c837c56aa5724d296bbccc0eebdbe1012cefc3c
Ibeh Esther <estheribeh5@gmail.com>
2022-09-07 23:23:45 +0100

    feat: Setup authentication and autorization

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const { User } = require('../models');
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+
+dotenv.config();
 
 const usersRouter = express.Router();
 
@@ -7,9 +11,42 @@ const hashPassword = () => {
 
 }
 
-usersRouter.post('/', async(req, res) => {
+const generateJWT =(data) => {
+    const jwt_secret = process.env.JWT_SECRET_KEY;
+    const token = jwt.sign(data, jwt_secret);
+    return token;
+}
+
+const get_token_from_header =(header, next) => {
+    if("authorization" in header){
+        console.log("Authorization in header");
+        const jwt_secret = process.env.JWT_SECRET_KEY;
+        const header_parts = header['authorization'].split(' ');
+        if(header_parts.length === 2){
+            if(header_parts[0] === "Bearer"){
+                return header_parts[1]
+            }else{
+                let err = new Error();
+                err.type = "bad request";
+                next(err);
+            }
+        }else{
+            let err = new Error();
+            err.type = "bad request";
+            next(err);
+        }
+    }else{
+        console.log("Authorization not in header")
+    }
+}
+
+const verify_jwt = (token) => {
+    console.log(token)
+}
+
+usersRouter.post('/', async(req, res, next) => {
     const { first_name, last_name, email, address,
-        phone_number, password } = req.body
+        phone_number, password } = req.body;
     
     try {
         if(first_name && last_name && email && address &&
@@ -20,23 +57,46 @@ usersRouter.post('/', async(req, res) => {
         }else{
             throw new Error();
         }
-        
     }catch (err) {
-        res.status(400).send(err)
+        err.type = "bad request";
+        next(err);
     }
 })
 
-usersRouter.get('/', async (req, res) => {
-    console.log(req.body)
-    const users = await User.find({...req.body});
+usersRouter.post('/login', async (req, res, next) => {
+    const {email, password} = req.body;
+    
     try{
-        if(users.length){
-            res.send(users);
+        if(email && password) {
+            // Validate user
+            const user = await User.findOne({...req.body});
+            // Generate JWT
+            const token = generateJWT(req.body);
+            if(user){
+                res.send({
+                    user,
+                    token
+                });
+            }else{
+                throw new Error();
+            }
         }else{
-            throw new Error();
+            let err = new Error();
+            err.type = "bad request";
+            next(err);
         }
+        
     }catch(err){
-        res.status(404).send(err)
+        err.type = "not found";
+        next(err);
+    }
+})
+
+usersRouter.get('/verify_jwt', (req, res, next) => {
+    const header = req.headers;
+    const token = get_token_from_header(header, next);
+    if(token){
+        verify_jwt(token);
     }
 })
 
